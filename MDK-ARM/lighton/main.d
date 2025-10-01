8c418224a4b6c102e2a432f7fa87d70e5df84eb8
yisicheng <289740868@qq.com>
2025-10-01 16:17:48 +0800

    change to timer trigger

--- MDK-ARM/lighton/main.d
+++ MDK-ARM/lighton/main.d
@@ -26,4 +26,6 @@ lighton/main.o: ..\Core\Src\main.c ..\Core\Inc\main.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_flash_ramfunc.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr_ex.h \
-  ..\Core\Inc\gpio.h
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim_ex.h \
+  ..\Core\Inc\tim.h ..\Core\Inc\gpio.h
--- MDK-ARM/lighton/tim.d
+++ MDK-ARM/lighton/tim.d
@@ -1,5 +1,4 @@
-lighton/stm32f4xx_hal_rcc.o: \
-  ..\Drivers\STM32F4xx_HAL_Driver\Src\stm32f4xx_hal_rcc.c \
+lighton/tim.o: ..\Core\Src\tim.c ..\Core\Inc\tim.h ..\Core\Inc\main.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h \
   ..\Core\Inc\stm32f4xx_hal_conf.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_rcc.h \
